a08495c165d725c9e3d0dbe54641cd3a2933a9b7
Emanuel Campos Jasso <isw.ecamposj@gmail.com>
2021-09-02 22:59:51 -0500

    se agrego el ejemplo del alamacemaiento en el local storage

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -14,21 +14,28 @@ export class GifsService {
     return [...this._history];
   }
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) {    
+    this._history = JSON.parse(localStorage.getItem('history')!) || [];
+    this.results = JSON.parse(localStorage.getItem('results')!) || [];
+  }
 
   findGifs(value: string) {
     value = value.trim().toLowerCase();
     if (!this._history.includes(value)) {
       this._history.unshift(value);
       this._history = this._history.splice(0, 9);
+
+      localStorage.setItem('history', JSON.stringify(this._history));
+
       this.http
         .get<GifsReponse>(
           `https://api.giphy.com/v1/gifs/search?api_key=${this.apyKey}&q=${value}&limit=10`
         )
-        .subscribe((response) =>{
+        .subscribe((response) => {
           this.results = response.data;
-          console.log(response);
+          localStorage.setItem('results', JSON.stringify(response.data));
         });
+
     }
   }
 }
